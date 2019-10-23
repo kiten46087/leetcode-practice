593f93019b7811a4b6f5bb2717cba60e5ffd1fbb
kiten46087 <592692725@qq.com>
2019-10-23 13:54:56 -0700

    Initial Exercise folder

---
+++ Solution717.java
@@ -0,0 +1,11 @@
+public class Solution717 {
+    public boolean isOneBitCharacter(int[] bits) {
+
+    }
+
+    public static void  main(String[] args) {
+        int[] input = new int[] {1, 1, 0};
+        Solution717 test = new Solution717();
+        System.out.println(test.isOneBitCharacter(input));
+    }
+}
