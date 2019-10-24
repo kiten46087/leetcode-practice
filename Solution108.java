6a1be3db0a29de05a1c1493104a20631fd6df950
kiten46087 <592692725@qq.com>
2019-10-24 15:09:05 -0700

    Add Solution 108

---
+++ Solution108.java
@@ -0,0 +1,25 @@
+public class Solution108 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode (int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) return null;
+        TreeNode res = helper(nums, 0, nums.length - 1);
+        return res;
+    }
+
+    public TreeNode helper(int[] nums, int left, int right) {
+        if (left > right) return null;
+        int mid = (left + right) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = helper(nums, left, mid - 1);
+        node.right = helper(nums, mid + 1, right);
+        return node;
+    }
+}
