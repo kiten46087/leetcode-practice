0114351a6971e8a0f27e01373f938f043c70087f
kiten46087 <592692725@qq.com>
2019-10-31 14:50:04 -0700

    Add solution 434

---
+++ Solution434.java
@@ -0,0 +1,22 @@
+import javax.swing.text.Segment;
+
+public class Solution434 {
+    public int countSegment(String s) {
+        // Use built-in function
+        // String trimmed = s.trim();
+        // if (trimmed.equals("")) {
+        //     return 0;
+        // }
+
+        // return trimmed.split("\\s+").length;
+
+        // Use count and in-place method
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if ((i == 0 || s.charAt(i - 1) == ' ') && s.charAt(i) != ' ') {
+                count += 1;
+            }
+        }
+        return count;
+    }
+}
