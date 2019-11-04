31ebad14637a373dd81fb71959dbac9f754e6fa0
kiten46087 <592692725@qq.com>
2019-11-04 15:07:31 -0800

    change Solution 538

--- Solution538.java
+++ Solution538.java
@@ -1,3 +1,4 @@
+import java.util.Stack;
 public class Solution538 {
     public class TreeNode {
         int val;
@@ -6,5 +7,36 @@ public class Solution538 {
         TreeNode (int x) {
             val = x;
         }
+    }
+
+    // int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        // Using stack process
+        int sum = 0;
+        TreeNode node = root;
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while (!stack.isEmpty() || node != null) {
+            while (node != null) {
+                stack.add(node);
+                node = node.right;
+            }
+
+            node = stack.pop();
+            sum += node.val;
+            node.val = sum;
+            
+            node = node.left;
+        }
+        return root;
+        // Using Recursive method
+        // if (root != null) {
+        //     convertBST(root.right);
+        //     sum += root.val;
+        //     root.val = sum;
+        //     convertBST(root.left);
+        // }
+
+        // return root;
     }
 }
