d79128ba543f897dc88160817639fa3a0d65a43d
kiten46087 <592692725@qq.com>
2019-12-16 16:29:48 -0800

    Add Solution12

--- Solution7.java
+++ Solution7.java
@@ -6,23 +6,24 @@ public class Solution7 {
         if (numRows == 1) return s;
 
         List<StringBuilder> rows = new ArrayList<>();
-        for (int i = 0; i < Math.min(numRows, s.length()); i++) {
+        for (int i = 0; i < Math.min(numRows, s.length(); i++) {
             rows.add(new StringBuilder());
-        }
+        } 
 
         int curRow = 0;
         boolean goingDown = false;
 
         for (char c : s.toCharArray()) {
             rows.get(curRow).append(c);
-            if (curRow == 0 || curRow == numRows - 1) {
-                goingDown = !goingDown;
-            }
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
             curRow += goingDown ? 1 : -1;
         }
 
         StringBuilder ret = new StringBuilder();
-        for (StringBuilder row : rows) ret.append(row);
+        for (StringBuilder row : rows) {
+            ret.append(row);
+        }
+
         return ret.toString();
     }
 }
