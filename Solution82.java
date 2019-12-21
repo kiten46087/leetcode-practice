99759b73e4a236e617d5f1cde1c536b13144b38a
kiten46087 <592692725@qq.com>
2019-12-21 14:11:02 -0800

    Add Solution82

---
+++ Solution82.java
@@ -0,0 +1,28 @@
+public class Solution82 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+        ListNode fakeHead = new ListNode(0);
+        fakeHead.next = head;
+        ListNode pre = fakeHead;
+        ListNode cur = head;
+        while (cur != null) {
+            while (cur.next != null && cur.val == cur.next.val) {
+                cur = cur.next;
+            }
+
+            if (pre.next == cur) {
+                pre = pre.next;
+            } else {
+                pre.next = cur.next;
+            }
+            cur = cur.next;
+        }
+        return fakeHead.next;
+    }
+}
