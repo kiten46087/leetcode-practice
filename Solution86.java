10609639d5be7981643ab8ddad65cb2166f58338
kiten46087 <592692725@qq.com>
2019-12-21 14:31:58 -0800

    Add Solution

---
+++ Solution86.java
@@ -0,0 +1,32 @@
+public class Solution86 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode before_head = new ListNode(0);
+        ListNode before = before_head;
+        ListNode after_head = new ListNode(0);
+        ListNode after = after_head;
+
+        while (head != null) {
+            if (head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                after.next = head;
+                after = after.next;
+            }
+
+            head = head.next;
+        }
+
+        after.next = null;
+        
+        before.next = after_head.next;
+
+        return before_head.next;
+    }
+}
