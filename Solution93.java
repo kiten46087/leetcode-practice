9f088ad222b470a2ad6586c257a171bfdc84f81e
kiten46087 <592692725@qq.com>
2019-12-22 13:55:29 -0800

    Add Solution93

---
+++ Solution93.java
@@ -0,0 +1,25 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution93 {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> res = new LinkedList<>();
+        helper(s, "", res, 0);
+        return res;
+    }
+
+    public void helper(String s, String temp, List<String> res, int n) {
+        if (n == 4) {
+            if (s.length() == 0) res.add(temp.substring(0, temp.length() - 1);
+            return;
+        }
+
+        for (int k = 1; k <= 3; k++) {
+            if (s.length() < k) continue;
+            int val = Integer.parseInt(s.substring(0, k));
+            if (val > 255 || k != String.valueOf(val).length()) continue;
+
+            helper(s.substring(k), temp + s.substring(0, k) + ".", res, n + 1);
+        }
+    }
+}
