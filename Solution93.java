dd41af58be4325af9274bc8c3d6ece85b04241e7
kiten46087 <592692725@qq.com>
2019-12-22 14:03:44 -0800

    Add Solution93

--- Solution93.java
+++ Solution93.java
@@ -10,12 +10,16 @@ public class Solution93 {
 
     public void helper(String s, String temp, List<String> res, int n) {
         if (n == 4) {
+            // substring here to get rid of the last '.'
             if (s.length() == 0) res.add(temp.substring(0, temp.length() - 1));
             return;
         }
 
         for (int k = 1; k <= 3; k++) {
             if (s.length() < k) continue;
+
+            // in the case 010 the parseInt will return len = 2, 
+            // where val = 10 but k = 3, skip this
             int val = Integer.parseInt(s.substring(0, k));
             if (val > 255 || k != String.valueOf(val).length()) continue;
 
