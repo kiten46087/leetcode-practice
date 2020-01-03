3d5dd18318d3000ca7b1df0a7e18f6048d1b9d9e
kiten46087 <592692725@qq.com>
2020-01-03 14:53:48 -0800

    Add Solution137

---
+++ Solution137.java
@@ -0,0 +1,17 @@
+public class Solution137 {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int sum = 0;
+            for (int n : nums) {
+                if ((n >> i & 1) == 1) {
+                    sum += 1;
+                }
+            }
+            sum = sum % 3;
+            res |= sum << i;
+        }
+
+        return res;
+    }
+}
