2e75c8c7b1db859b75d752f02a48c037aec014ad
kiten46087 <592692725@qq.com>
2020-01-09 15:33:49 -0800

    add a not very good solution of 215

--- Solution215.java
+++ Solution215.java
@@ -1,18 +1,36 @@
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class Solution215 {
     public int findKthLargeast(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (pq.size() < k) {
-                pq.add(nums[i]);
-            } else {
-                if (nums[i] > pq.peek()) {
-                    pq.remove();
-                    pq.add(nums[i]);
-                }
-            }
+
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (pq.size() < k) {
+        //         pq.add(nums[i]);
+        //     } else {
+        //         if (nums[i] > pq.peek()) {
+        //             pq.remove();
+        //             pq.add(nums[i]);
+        //         }
+        //     }
+        // }
+        // return pq.remove();
+
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int num : nums) {
+            pq.add(num);
         }
-        return pq.remove();
+
+        int count = 0;
+        int res = 0;
+        while (count < k) {
+            res = pq.poll();
+            count += 1;
+        }
+
+        return res;
     }
 }
