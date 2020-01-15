0e87eb8ce043eed92e09bfbd9c99048b7efddf81
kiten46087 <592692725@qq.com>
2020-01-14 16:38:03 -0800

    Add Solution264

---
+++ Solution264.java
@@ -0,0 +1,21 @@
+public class Solution264 {
+    public int nthUglyNumber(int n) {
+        int[] nums = new int[n];
+        int index2 = 0;
+        int index3 = 0;
+        int index5 = 0;
+        nums[0] = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = Math.min(nums[index2] * 2, Math.min(nums[index3] * 3, nums[index5] * 5));
+            if (nums[i] == nums[index2] * 2)
+                index2 += 1;
+            if (nums[i] == nums[index3] * 3)
+                index3 += 1;
+            if (nums[i] == nums[index5] * 5)
+                index5 += 1;
+        }
+
+        return nums[n - 1];
+    }
+}
